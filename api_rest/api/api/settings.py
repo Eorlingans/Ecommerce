313b00ee1b6b7fc761b97d2a69b254111cc30ef7
Lucio Rotelli <devluciorotelli@gmail.com>
2021-12-21 14:09:27 -0300

    FINALL
    FINAL

--- api_rest/api/api/settings.py
+++ api_rest/api/api/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
             'USER': 'root',
             'PASSWORD': 'root',
             'HOST': 'localhost',
-            'PORT': '3306',
+            'PORT': '3307',
         }
 }
 
